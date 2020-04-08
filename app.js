3df445420a5dd50de9d6e10a02785323cd073930
geraldi16 <geraldi.anggapardana@online-pajak.com>
2020-04-08 17:06:11 +0700

    learning vuejs until module 16

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ new Vue({
         event: {
             eventDate: 'August 14th - 16th',
             eventTitle: 'Summer Festival!',
+            signUpText: 'Add your name to the guest list for <em>exclusive</em> offers:',
             eventDescription: 'It\'s back! This years summer festival will be in the beautiful countryside featuring our best line up ever!',
         },
         newEventName: '',
         guestName: [],
+        formSubmitClass: "",
+        customStyles: {
+            margin: '10px',
+            color: 'blue'
+        },
     },
     methods: {
         formSubmitted: function() {
+            if (this.newEventName.length === 0) return
+
             this.guestName.push(this.newEventName)
             this.newEventName = ''
+            this.formSubmitClass = 'submitted'
         }
     }
 })
